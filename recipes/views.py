f051f8eea56316dd617795ff3f9bcc23aa177849
mqsalx <mqsalx@gmail.com>
2023-06-22 12:33:05 -0300

    Update

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# flake8: noqa
 
+from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse('Home')
+
+def sobre(request):
+    return HttpResponse('Sobre')
