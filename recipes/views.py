7284a836e56476b2bb454e1de879ceede55f32b8
mqsalx <mqsalx@gmail.com>
2023-06-23 07:55:40 -0300

    Update

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html', context= {
+    return render(request, 'recipes/pages/home.html', context= {
         'name': 'Receitas'
     })
 
