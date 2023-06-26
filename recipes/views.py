4e9e2fd49fdd161ac187f2a2053e919db7146938
mqsalx <mqsalx@gmail.com>
2023-06-26 11:22:43 -0300

    Templates

--- recipes/views.py
+++ recipes/views.py
@@ -8,4 +8,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context= {
         'name': 'Receitas'
     })
+    
 
+def recipe(request,id):
+    return render(request, 'recipes/pages/recipe-view.html', context= {
+        'name': 'Receitas'
+    })
