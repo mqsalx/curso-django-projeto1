b3ca98c8eb4c0713ec02b6b8f3c34ded5ff06b79
mqsalx <mqsalx@gmail.com>
2023-06-26 15:56:28 -0300

    Inject data with faker

--- recipes/views.py
+++ recipes/views.py
@@ -2,15 +2,18 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 # Create your views here.
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context= {
-        'name': 'Receitas'
+        'name': 'Receitas',
+        'recipes': [make_recipe() for _ in range(10)],
     })
     
 
 def recipe(request,id):
     return render(request, 'recipes/pages/recipe-view.html', context= {
-        'name': 'Receitas'
+        'name': 'Receitas',
+        'recipe': make_recipe(),
     })
