113e0e76768271ae820b61b3ee1b43754c40ca37
mqsalx <mqsalx@gmail.com>
2023-06-30 09:53:54 -0300

    Updated files

--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 # Create your views here.
 
-def home(request):
-    return render(request, 'recipes/pages/home.html', context= {
+def home(request):    
+    recipes = Recipe.objects.filter(
+        is_published=True
+        ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
         'name': 'Receitas',
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
     
 
