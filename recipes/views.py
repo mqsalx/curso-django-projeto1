2f16060d54b2823a73af37a4f3f319cabd600d9c
mqsalx <mqsalx@gmail.com>
2023-06-30 12:35:45 -0300

    Updated files

--- recipes/views.py
+++ recipes/views.py
@@ -1,39 +1,39 @@
 # flake8: noqa
-from django.http import Http404
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_list_or_404, render
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 # Create your views here.
 
-def home(request):    
+def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
-        ).order_by('-id')
-    
-    if not recipes:
-        raise Http404('Not found 🥲')
-    
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category | '
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,
-    ).order_by('-id')
-    return render(request, 'recipes/pages/category.html', context={
-        'name': 'Receitas',
-        'recipes': recipes,
-    })
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
     
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
+    })
 
 def recipe(request,id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+    
     return render(request, 'recipes/pages/recipe-view.html', context= {
         'name': 'Receitas',
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
