9cdd0ee6a7c387782834c60295584ebd28b1142b
mqsalx <mqsalx@gmail.com>
2023-07-03 14:00:16 -0300

    Updated files

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 # flake8: noqa
+from django.db.models import Q
 from django.shortcuts import get_list_or_404, get_object_or_404, render
-from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 from django.http.response import Http404
 # Create your views here.
@@ -41,7 +41,17 @@ def search(request):
         
     if not search_term:
         raise Http404()
+    
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+    
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
+        'recipes': recipes,
     })
