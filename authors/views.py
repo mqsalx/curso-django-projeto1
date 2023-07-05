8682291345c9f93498b581e9d3df1ad98e5b9d6e
mqsalx <mqsalx@gmail.com>
2023-07-05 15:04:57 -0300

    Updated files

--- authors/views.py
+++ authors/views.py
@@ -1,7 +1,8 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
+
 from .forms import RegisterForm
-from django.contrib import messages
 
 
 def register_view(request):
@@ -24,6 +25,6 @@ def register_create(request):
         form.save()
         messages.success(request, 'Your user is created, please log in.')
 
-    del (request.session['register_form_data'])
+        del (request.session['register_form_data'])
 
     return redirect('authors:register')
